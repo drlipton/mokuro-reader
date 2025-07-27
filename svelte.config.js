310fd0cbebeb5bddc109939ec853cd0c38c9e1b6
Joel <sussuke@gmail.com>
2025-07-27 02:29:23 +0200

    changed adapter settings

--- svelte.config.js
+++ svelte.config.js
@@ -1,23 +1,15 @@
-import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'; // Correct this line
+import adapter from '@sveltejs/adapter-node'; // Change this line
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: vitePreprocess(),
-
-  kit: {
-    adapter: adapter({
-      pages: 'build',
-      assets: 'build',
-      fallback: 'index.html',
-      precompress: false,
-      strict: true
-    })
-  },
-
-  vitePlugin: {
-    inspector: true
-  }
+	preprocess: vitePreprocess(),
+	kit: {
+		adapter: adapter() // And change this line
+	},
+	vitePlugin: {
+		inspector: true
+	}
 };
 
 export default config;
